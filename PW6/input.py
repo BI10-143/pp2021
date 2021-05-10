176f36788d937d0bc229acd757e8c841aa90aff0
BI10-143 <74813411+BI10-143@users.noreply.github.com>
2021-05-10 15:05:54 +0700

    Update input.py

--- PW6/input.py
+++ PW6/input.py
@@ -5,7 +5,7 @@ def enter_info(self):
         self.name = input("Enter Student name please:" )
         self.id = input("Enter Student ID please:" )
         self.dob = input("Enter Student DOB please:" )
-        return print(f"Student Info: \t{self.name} \t {self.id} \t {self.dob} \n ----------"  )
+        return print(f"Student Info: \t{self.name} \t {self.id} \t {self.dob}"  )
 
      def str(self):
         return  f"- Student Name: {self.name}" 
@@ -18,11 +18,11 @@ def enter_info(self):
 def enter_info(self):
         self.name = input("Enter Student name please:" )
         self.id = input("Enter Student ID please:" )
-        return print(f"Course Info: \t{self.name} \t {self.id} \n ----------"  )
+        return print(f"Course Info: \t{self.name} \t {self.id}"  )
 
      def str(self):
         return  f"- Student Name: {self.name}" 
-                f"- Student ID: {self.id}"
+        return  f"- Student ID: {self.id}"
         
     def str(self):
         print(self.str())
@@ -42,6 +42,6 @@ def enter_info(self):
             m = []
             for j in range(len(self.courses)):
                 course = int(input(
-                    f"-Enter mark of student name {self.students[i].student_name()} in course {self.courses[j].course_information()}: "))
+                    f"Please Enter mark of student name {self.students[i].student_name()} in course {self.courses[j].course_information()}: "))
                 m += [course]
             self.marksheets += [m]s
